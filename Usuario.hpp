02a0e31c750af4679b0ef72d737fd374ad8f6a5d
1felipemartins1 <129700548+1felipemartins1@users.noreply.github.com>
2023-10-25 21:41:29 -0300

    rs

--- Usuario.hpp
+++ Usuario.hpp
@@ -1,18 +1,13 @@
 #ifndef USUARIO_
 #define USUARIO_
-
+#include <string>
+#include <iostream>
 using namespace std;
 
 
 
 Class Usuario{
-<<<<<<< HEAD
     string nome; 
 }
-=======
-    int cpf;
-    
-};
->>>>>>> 017cba10ba65b28fd51d3396fec0e32fc717e74a
 
 #endif USUARIO_
