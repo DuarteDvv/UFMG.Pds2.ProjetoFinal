cadf5e2689ee2305909636298a98c5bf247355fa
DuarteDvv <136333571+DuarteDvv@users.noreply.github.com>
2023-10-29 10:03:44 -0300

    Update iUsuario.cpp

--- iUsuario.cpp
+++ iUsuario.cpp
@@ -1,1 +1,13 @@
+#include "Usuario.hpp"
+#include <list>
 
+void Usuario::setUsuario(std::string nome, int cpf){ //Aloca os dados e verifica se já existe
+    if (!função de busca em lista){
+        this->nome = nome;
+        this->cpf = cpf;
+        std::cout << "Cliente registrado" << std::endl;
+    }
+    else{
+        std::cout << "Cliente já existe" << std::endl;
+    }
+}
