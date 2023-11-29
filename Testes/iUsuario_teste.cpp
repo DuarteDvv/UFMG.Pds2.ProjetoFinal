5732409f64775c70cc9c3b937e373411fa9c5a1d
Luiz Felipe Costa <91031579+luizcostaxp@users.noreply.github.com>
2023-11-29 08:06:56 -0300

    em desenvolvimento (12)

--- Testes/iUsuario_teste.cpp
+++ Testes/iUsuario_teste.cpp
@@ -1,1 +1,13 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "Usuario.hpp"
+
+TEST_CASE("Obter CPF do usuário")
+{
+        std::string nome = "Ludovicus";
+        std::string cpf = "00100100101";
+        Usuario* u = new Usuario(nome, cpf); 
+        
+        CHECK(u->getCPF() == "00100100101");
+}
 
