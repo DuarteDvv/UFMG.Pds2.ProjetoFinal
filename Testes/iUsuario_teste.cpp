ded4caad31e60a2d3a77788df99d236a092a2f56
Luiz Felipe Costa <91031579+luizcostaxp@users.noreply.github.com>
2023-11-29 08:21:03 -0300

    em desenvolvimento (14)

--- Testes/iUsuario_teste.cpp
+++ Testes/iUsuario_teste.cpp
@@ -20,3 +20,14 @@ TEST_CASE("Obter Nome do usuário")
         CHECK(u->getNome()=="Ludovicus");
 }
 
+TEST_CASE("Obter número de acessos do usuário")
+{
+        std::string nome = "Ludovicus";
+        std::string cpf = "00100100101";
+        Usuario* u = new Usuario(nome, cpf);
+
+        CHECK(u->getAcessos()==1);
+        u->AcessAdd(25);
+        CHECK(u->getAcessos()==26);
+}
+
