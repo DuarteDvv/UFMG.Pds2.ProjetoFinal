d2643bc3d3feafc17086166a5a1a74a531412f16
Luiz Felipe Costa <91031579+luizcostaxp@users.noreply.github.com>
2023-11-28 23:57:29 -0300

    em desenvolvimento(1)

--- Testes/TesteiFilmCPP.cpp
+++ Testes/TesteiFilmCPP.cpp
@@ -1,13 +1,47 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "iFilm.cpp"
+#include "system.hpp"
 
-//Criar objeto
-//Verificar existencia
+TEST_CASE("Cálculo de Preço para diferentes tipos de filme") {
 
-TEST_CASE("Calculo de preço - Operações básicas") 
-{
-CHECK(CalcularPreco(10) == 100);
-CHECK(CalcularPreco(-10) == 0);
-CHECK(CalcularPreco(0) == 0);
+    /* -------------------------------------------------------------------
+        Adendo: A hipótese em que a quantidade de dias inserida é negativa
+        não foi inserida neste escopo pois essa exceção é tratada ainda
+        na função main.    
+    ---------------------------------------------------------------------- */
+
+    SUBCASE("Tipo Estoque") 
+    {
+        Sistema sistema;
+        std::string TituloTeste = "Título teste";
+        std::string TipoTeste = "estoque";
+        filme* d = new dvd(123,TituloTeste,1,TipoTeste); 
+        sistema.CadastrarFilme(d);
+
+        CHECK(d->CalcularPreco(5) == doctest::Approx(10.0 * 5));
+    }
+
+     SUBCASE("Tipo Promocao") 
+    {
+        Sistema sistema;
+        std::string TituloTeste = "Título teste";
+        std::string TipoTeste = "promocao";
+        filme* d = new dvd(123,TituloTeste,1,TipoTeste); 
+        sistema.CadastrarFilme(d);
+
+        CHECK(d->CalcularPreco(5) == doctest::Approx(10.0));
+    }
+
+    SUBCASE("Tipo Lançamento") 
+    {
+        Sistema sistema;
+        std::string TituloTeste = "Título teste";
+        std::string TipoTeste = "lancamento";
+        filme* d = new dvd(123,TituloTeste,1,TipoTeste); 
+        sistema.CadastrarFilme(d);
+
+        CHECK(d->CalcularPreco(5) == doctest::Approx(100.0));
+    }
+
+
 }
