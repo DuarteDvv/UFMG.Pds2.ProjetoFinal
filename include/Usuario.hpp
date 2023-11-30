073a308af8a8a02f13fbf278956c598b91d01136
Felipe Martins <129700548+1felipemartins1@users.noreply.github.com>
2023-11-29 21:44:05 -0300

    comentarios usuario hpp

--- include/Usuario.hpp
+++ include/Usuario.hpp
@@ -1,43 +1,48 @@
 #ifndef USUARIO_
 #define USUARIO_
+
 #include <string>
 #include <iostream>
 #include <list>
 #include "Film.hpp"
 
+class Usuario {
+private:
+    std::string nome;
+    std::string cpf;
+    int acessos;
+    std::list<filme*> Carrinho;
 
-class Usuario{
-    private:
-        std::string nome;
-        std::string cpf;
-        int acessos;
+public:
+    // Construtor da classe Usuario, inicializa nome, cpf e acessos
+    Usuario(std::string nome, std::string cpf) : nome(nome), cpf(cpf) {
+        this->acessos = 1;
+    } 
 
-        std::list<filme*> Carrinho;
-    
-    public:
-        Usuario(std::string nome, std::string cpf) : nome(nome), cpf(cpf) {
-            this->acessos = 1;
-        } 
-
-        ~Usuario() {
+    // Destrutor da classe Usuario, libera a memória alocada para filmes no carrinho
+    ~Usuario() {
         for (auto* filmePtr : Carrinho) {
             delete filmePtr;
         }
         Carrinho.clear();
-        }
-        
-        std::string getCPF();
-        std::string getNome(); 
-        int getAcessos();
-        void AcessAdd(int);
-        void addCarrinho(filme*);
-        void ListarCarrinho();
-        void recibo(int &);
+    }
+    
+    // Métodos de acesso aos membros privados
+    std::string getCPF();
+    std::string getNome(); 
+    int getAcessos();
 
+    // Método para adicionar acessos
+    void AcessAdd(int);
 
+    // Método para adicionar filme ao carrinho
+    void addCarrinho(filme*);
+
+    // Método para listar o conteúdo do carrinho
+    void ListarCarrinho();
+
+    // Método para gerar um recibo de compra
+    void recibo(int &);
 }; 
+
 #endif
-
-
-        
- 
