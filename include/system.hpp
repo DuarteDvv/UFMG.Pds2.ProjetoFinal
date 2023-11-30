71131cdef352cd2dc1096c885214f6c1a3accc27
Felipe Martins <129700548+1felipemartins1@users.noreply.github.com>
2023-11-29 21:37:30 -0300

    comentarios system hpp

--- include/system.hpp
+++ include/system.hpp
@@ -10,13 +10,16 @@
 #include <thread>
 #include <chrono>
 
+// Definição da classe Sistema
 class Sistema
 {
 private:
+    // Listas para armazenar objetos de usuário e filmes
     std::list<Usuario *> Usuarios;
     std::list<filme *> Catalogo;
 
 public:
+    // Destrutor da classe, responsável por liberar a memória alocada para usuários e filmes
     ~Sistema()
     {
         for (auto &usuarioPtr : Usuarios)
@@ -30,50 +33,50 @@ public:
         }
     }
 
-    // User Methods
-    void TratarCPF(std::string &);
-    void TratarNome(std::string &);
-    void TratarExistenciaU(std::string &);
-    void TratarListagemU(std::string &);
-    bool VerificarCpf(std::string &);
-    bool VerificarNome(std::string &);
-    bool ExisteUsuario(std::string &);
-    void CadastrarCliente(Usuario *&);
-    void RemoverCliente(std::string &);
-    void ListarCliente(std::string &);
+    // Métodos relacionados aos usuários
+    void TratarCPF(std::string &); // Trata o CPF para consistência
+    void TratarNome(std::string &); // Trata o nome para consistência
+    void TratarExistenciaU(std::string &); // Trata a existência de um usuário
+    void TratarListagemU(std::string &); // Trata a listagem de usuários
+    bool VerificarCpf(std::string &); // Verifica a validade do CPF
+    bool VerificarNome(std::string &); // Verifica a validade do nome
+    bool ExisteUsuario(std::string &); // Verifica se um usuário existe
+    void CadastrarCliente(Usuario *&); // Cadastra um novo cliente
+    void RemoverCliente(std::string &); // Remove um cliente
+    void ListarCliente(std::string &); // Lista os clientes
 
-    // Film Methods
-    void TratarTIPO(std::string &);
-    void TratarQuantidade(int &);
-    void TratarCod(int &);
-    void TratarTitulo(std::string &);
-    void CadastrarFilme(filme *&);
-    bool VerificaTitulo(std::string &);
-    void TratarCat(std::string &);
-    void TratarCodN(int &);
-    void TratarListagemF(std::string &);
-    void TratarExistenciaF(int &);
-    bool ExisteFilme(int &);
-    void ListarFilmes(std::string &);
-    void RemoverFilme(int &);
-    void AdicionarCarrinho(int &, std::string &);
-    void ListarCompras(std::string &);
-    void Recibo(std::string &, int &);
-    void CadastrarFilmesDoArquivo(std::ifstream &);
+    // Métodos relacionados aos filmes
+    void TratarTIPO(std::string &); // Trata o tipo de filme
+    void TratarQuantidade(int &); // Trata a quantidade de filmes
+    void TratarCod(int &); // Trata o código do filme
+    void TratarTitulo(std::string &); // Trata o título do filme
+    void CadastrarFilme(filme *&); // Cadastra um novo filme
+    bool VerificaTitulo(std::string &); // Verifica a existência de um título
+    void TratarCat(std::string &); // Trata a categoria do filme
+    void TratarCodN(int &); // Trata o código do filme para listagem
+    void TratarListagemF(std::string &); // Trata a listagem de filmes
+    void TratarExistenciaF(int &); // Trata a existência de um filme
+    bool ExisteFilme(int &); // Verifica se um filme existe
+    void ListarFilmes(std::string &); // Lista os filmes
+    void RemoverFilme(int &); // Remove um filme
+    void AdicionarCarrinho(int &, std::string &); // Adiciona filme ao carrinho
+    void ListarCompras(std::string &); // Lista as compras realizadas
+    void Recibo(std::string &, int &); // Gera um recibo de compra
+    void CadastrarFilmesDoArquivo(std::ifstream &); // Cadastra filmes a partir de um arquivo
 
-    // Extra
-    void TratarOE(std::string &);
-    void TratarSenha(std::string &);
-    void AnimCarregarDados();
-    void animacaoSalvando();
-    void AnimacaoEscolha();
-    void LoadData();
-    void SaveData();
-    void ListarEstoque();
-    void RegistroDeAlugays();
-    void incrementarEstoque(int &, int &);
-    void decrementarEstoque(int &, int &);
-    void SugerirFilme();
+    // Métodos adicionais
+    void TratarOE(std::string &); // Trata opções de estoque
+    void TratarSenha(std::string &); // Trata senha
+    void AnimCarregarDados(); // Animação de carregamento de dados
+    void animacaoSalvando(); // Animação de salvamento de dados
+    void AnimacaoEscolha(); // Animação de escolha
+    void LoadData(); // Carrega dados
+    void SaveData(); // Salva dados
+    void ListarEstoque(); // Lista o estoque
+    void RegistroDeAlugays(); // Registra aluguéis
+    void incrementarEstoque(int &, int &); // Incrementa o estoque de um filme
+    void decrementarEstoque(int &, int &); // Decrementa o estoque de um filme
+    void SugerirFilme(); // Sugere um filme
 };
 
 #endif
