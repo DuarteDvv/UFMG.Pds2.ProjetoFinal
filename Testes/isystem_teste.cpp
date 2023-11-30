e66b8dfdb3416888c0f96f10d92739fda4dfe2a7
Luiz Felipe Costa <91031579+luizcostaxp@users.noreply.github.com>
2023-11-30 12:33:27 -0300

    Update isystem_teste.cpp

--- Testes/isystem_teste.cpp
+++ Testes/isystem_teste.cpp
@@ -2,6 +2,7 @@
 #include "doctest.h"
 #include "system.hpp"
 #include "Usuario.hpp"
+#include "Film.hpp"
 #include <sstream>
 
 TEST_CASE("Verificação de CPF")
@@ -126,3 +127,82 @@ TEST_CASE("Remover Cliente")
         sistema.RemoverCliente(cpf);
         CHECK(sistema.ExisteUsuario(cpf)==false); 
 }
+
+TEST_CASE("Listar clientes")
+{
+    Sistema sistema;
+    std::string nome = "Ludovicus";
+    std::string cpf = "00100100101";
+    Usuario* u = new Usuario(nome, cpf);
+    std::string cat ="N"; 
+    std::stringstream buffer;
+    std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+
+    sistema.CadastrarCliente(u);
+    sistema.ListarCliente(cat);
+    std::cout.rdbuf(old);
+    std::string saida = buffer.str();
+
+    CHECK(saida=="|| 00100100101 || >> Ludovicus\n");
+
+}
+
+TEST_CASE("Verificar título")
+{
+    SUBCASE("Título válido")
+    {
+    Sistema sistema;
+    std::string tit = "Titulo de Teste";
+    CHECK(sistema.VerificaTitulo(tit)==true);
+    }
+
+    SUBCASE("Título inválido")
+    {
+    Sistema sistema;
+    std::string tit = "Teste/////";
+    CHECK(sistema.VerificaTitulo(tit)==false);
+    }
+}
+
+TEST_CASE("Cadastrar filme e Existe filme")
+{
+    Sistema sistema;
+    int cod=456;
+    filme* e = new dvd(cod,"Título teste II",1,"promocao");
+    sistema.CadastrarFilme(e);
+    CHECK(sistema.ExisteFilme(cod)==true);
+    
+}
+
+TEST_CASE("Listar filmes")
+{
+    Sistema sistema;
+    int cod=456;
+    std::string cmd = "T";
+    std::stringstream buffer;
+    std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+    filme* e = new dvd(cod,"Título teste II",1,"promocao");
+    sistema.CadastrarFilme(e);
+    sistema.ListarFilmes(cmd);
+    std::cout.rdbuf(old);
+    std::string saida = buffer.str();
+
+    CHECK(saida=="||456|| >> Título teste II\n");
+}
+
+TEST_CASE("Remoção de filme")
+{
+     Sistema sistema;
+    int cod=456;
+    filme* e = new dvd(cod,"Título teste II",1,"promocao");
+    sistema.CadastrarFilme(e);
+    sistema.RemoverFilme(cod);
+    
+    CHECK(sistema.ExisteFilme(cod)==false);
+}
+
+/*
+    Adendo: Os demais métodos desta classe foram suprimidos
+    nos testes por cumprirem funções auxiliares ou apenas
+    reproduzirem o funcionamento das aqui já testadas. 
+*/
